d69c5f4082e1d2bd832e4fb98bc2df16a8cae3df
ghingst <ghingst@cedarville.edu>
2022-05-02 17:17:55 -0400

    term/year formatting better

--- src/components/Term2.js
+++ src/components/Term2.js
@@ -3,42 +3,25 @@ const Term2 = (props)=>{
     let courses = Object.entries(courseList).map(([key,value]) => {
         const courseName = key+ " " + props.catalog.courses[key].name;
         return (
-            <p>
-            {courseName}
-            </p>
+            <div className="courseWrapper" draggable='true' ondragstart='dragStart(event, true)'>
+                <p className="course">
+                {courseName}
+                <img class='del-course' src='Images/x-icon.png'/>
+                </p>
+            </div>
         );	
     });
 
     return (
-        <div>
-        {courses}
+        <div className="semester">
+            <span className="period" ondragover='dragover(event)' ondrop='drop(event, this)'>
+                <div class ="period-header">
+                    <span className="period-header-semester">{props.term} {props.year}</span>
+                    <span className="period-header-credits">Credits: </span>
+                </div>   
+            </span>
+            {courses}
         </div>
-
-
-
-
-//Fall " + yearArray.at(i).year + "</span>";
-
-
-
-//         //html += "<div style='width: 100%'><span>Fall " + yearArray.at(i).year + "</span>";
-//         //html += "<span style='position: absolute; right: 0'>credits: 10</span></div>";
-// for (var j = 0; j < yearArray[i].fallTerm.courses.length; j++) {
-//     for (var k = 0; k < catalog.courses.length; k++) {
-//         if (catalog.courses[k].id == yearArray[i].fallTerm.courses[j].courseDesignator) {
-//             credits = parseInt(catalog.courses[k].credits);
-//             courseName = yearArray[i].fallTerm.courses[j].courseDesignator + " " + catalog.courses[k].name;
-//             html += "<div class='courseWrapper' courseName='" + courseName + "' credits='" + credits + "' draggable='true' ondragstart='dragStart(event, true)'>"
-//                 +       "<p class='course'>"
-//                 +           courseName 
-//                 +           "<img class='del-course' src='Images/x-icon.png'/>"
-//                 +       "</p>"
-//                 +   "</div>";
-//         }
-//     }
-// }
-
-
     );
 
 }
--- src/components/Year.js
+++ src/components/Year.js
@@ -6,37 +6,10 @@ class Year extends Component {
 	render (){
 	  return (
 		<div className="year">
-			{/* <h3>{this.props.year}</h3> */}
-			<div className="semester">
-				<span className="period" ondragover='dragover(event)' ondrop='drop(event, this)'>
-					<div class ="period-header">
-						<span className="period-header-semester">Fall</span>
-						<span className="period-header-credits"></span>
-					</div>
-					
-				</span>
-				<Term2 catalog={this.props.catalog} data={this.props.data['fa']} term="Fall" />
-			</div>
-			<div className="semester">
-				<span className="period" ondragover='dragover(event)' ondrop='drop(event, this)'>
-					<div class ="period-header">
-						<span className="period-header-semester">Fall</span>
-						<span className="period-header-credits"></span>
-					</div>
-					
-				</span>
-				<Term2 catalog={this.props.catalog} data={this.props.data['sp']} term="Spring" />			</div>
-			<div className="semester">
-				<span className="period" ondragover='dragover(event)' ondrop='drop(event, this)'>
-					<div class ="period-header">
-						<span className="period-header-semester">Fall</span>
-						<span className="period-header-credits"></span>
-					</div>
-					
-				</span>
-				<Term2 catalog={this.props.catalog} data={this.props.data['su']} term="Summer" />
-			</div>
-				
+			{/* <h3>{this.props.year}</h3> */}			
+			<Term2 catalog={this.props.catalog} data={this.props.data['fa']} term="Fall" year={this.props.year}/>
+			<Term2 catalog={this.props.catalog} data={this.props.data['sp']} term="Spring" year={this.props.year}/>
+			<Term2 catalog={this.props.catalog} data={this.props.data['su']} term="Summer" year={this.props.year}/>	
 		</div>
 	  );
 	}
