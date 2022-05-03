971874038012e3cb029f5d1221e163e9effca0a2
jacobpennell <jacobpennell@cedarville.edu>
2022-05-02 21:10:51 -0400

    LowerRight changes

--- src/components/LowerRight.js
+++ src/components/LowerRight.js
@@ -5,7 +5,22 @@ class LowerRight extends Component {
     render () {
         return (
         <div id="course-finder">
-                
+            <div>
+            </div>
+            <table id= "catalog_table" className="catalogDisplay">
+            <thead>
+            <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Credits</th>
+            </tr>
+            </thead>
+            <tbody>
+
+           
+            </tbody>
+            </table>
         </div>
         );
     }
